7a5eba5a45fb74867a9e04d472591b8faa31b1a3
Anton Mihaylov <lucidsensedev@gmail.com>
2020-11-13 13:28:47 +0200

    Added nested image test

--- OpenXMLTemplatesTest/ControlReplacersTests/PictureControlReplacerTests/Tests.cs
+++ OpenXMLTemplatesTest/ControlReplacersTests/PictureControlReplacerTests/Tests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.IO;
 using NUnit.Framework;
 using OpenXMLTemplates;
@@ -5,28 +6,35 @@ using OpenXMLTemplates.ControlReplacers;
 using OpenXMLTemplates.Documents;
 using OpenXMLTemplates.Variables;
 
-namespace OpenXMLTempaltesTest.ControlReplacersTests.PictureControlReplacerTests {
-    public class Tests {
+namespace OpenXMLTempaltesTest.ControlReplacersTests.PictureControlReplacerTests
+{
+    public class Tests
+    {
         private TemplateDocument GetDoc => new TemplateDocument(this.CurrentFolder() + "Doc.docx");
         private string GetData => File.ReadAllText(this.CurrentFolder() + "data.json");
 
         [Test]
-        public void TestVariableControls() {
+        public void TestVariableControls()
+        {
             using var doc = GetDoc;
             var data = GetData;
 
-            var src = new VariableSource();
-            src.LoadDataFromJson(data);
+            var src = new VariableSource(data);
 
-            // Substitue path for testing 
+            // Substitute path for testing 
             src.Data["picture1"] = this.CurrentFolder() + "/" + src.Data["picture1"];
+
+            if (src.Data["nested"] is IDictionary nested)
+                nested["picture1"] = this.CurrentFolder() + "/" + src.Data["picture1"];
 
             var replacer = new PictureControlReplacer();
 
             replacer.ReplaceAll(doc, src);
             doc.SaveAs(this.CurrentFolder() + "result.docx");
 
-            Assert.AreEqual("DocumentFormat.OpenXml.Wordprocessing.SdtBlock", doc.WordprocessingDocument.FindContentControl(replacer.TagName + "_" + "picture1").GetType().ToString());
+            Assert.AreEqual("DocumentFormat.OpenXml.Wordprocessing.SdtBlock",
+                doc.WordprocessingDocument.FindContentControl(replacer.TagName + "_" + "picture1").GetType()
+                    .ToString());
 
             doc.WordprocessingDocument.AssertValid();
         }
