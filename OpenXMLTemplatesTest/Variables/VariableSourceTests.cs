59bef706e4c49bbadc3c70db379a16a1dfda4d7d
Don Waldo <dwaldo@agmotion.com>
2021-09-14 11:01:20 -0500

    Adds numeric formatting

--- OpenXMLTemplatesTest/Variables/VariableSourceTests.cs
+++ OpenXMLTemplatesTest/Variables/VariableSourceTests.cs
@@ -5,30 +5,58 @@ using OpenXMLTemplates.Variables.Exceptions;
 
 namespace OpenXMLTempaltesTest
 {
-    public class DataTests
-    {
-        
-        [Test]
-        public void GetVariableByIdentifierWorksCorrectly()
-        {
-            var addressDict = new Dictionary<string, string> {{"street", "MyStreet"}, {"number", "1"}, {"app", "2"}};
+	public class DataTests
+	{
+
+		[Test]
+		public void GetVariableByIdentifierWorksCorrectly()
+		{
+			var addressDict = new Dictionary<string, string> { { "street", "MyStreet" }, { "number", "1" }, { "app", "2" } };
 
 
-            var data = new Dictionary<string, object>
-            {
-                {"name", "MyName"}, {"address", addressDict}, {"phones", new List<string> {"123", "12345"}}
-            };
+			var data = new Dictionary<string, object>
+			{
+				{"name", "MyName"}, {"address", addressDict}, {"phones", new List<string> {"123", "12345"}}
+			};
 
-            var source = new VariableSource(data);
-            
-            Assert.AreEqual("MyName", source.GetVariable<string>("name"));
-            Assert.AreEqual("MyStreet", source.GetVariable<string>("address.street"));
+			var source = new VariableSource(data);
 
-            Assert.AreEqual("12345", source.GetVariable<string>("phones.[1]"));
+			Assert.AreEqual("MyName", source.GetVariable<string>("name"));
+			Assert.AreEqual("MyStreet", source.GetVariable<string>("address.street"));
 
-            Assert.Throws<VariableNotFoundException>((() => source.GetVariable<string>("name.street")));
-            Assert.Throws<VariableNotFoundException>((() => source.GetVariable<string>("address.streeets")));
-            Assert.Throws<IncorrectVariableTypeException>((() => source.GetVariable<int>("name")));
-        }
-    }
+			Assert.AreEqual("12345", source.GetVariable<string>("phones.[1]"));
+
+			Assert.Throws<VariableNotFoundException>((() => source.GetVariable<string>("name.street")));
+			Assert.Throws<VariableNotFoundException>((() => source.GetVariable<string>("address.streeets")));
+			Assert.Throws<IncorrectVariableTypeException>((() => source.GetVariable<int>("name")));
+		}
+
+		[Test]
+		public void Format_Numeric_Fields_Value_Null()
+		{
+
+			var data = new Dictionary<string, object>
+			{
+				{ "prices", null }
+			};
+
+			var source = new VariableSource(data);
+
+			Assert.AreEqual("", source.GetVariable<string>("prices(N2)"));
+		}
+
+		[Test]
+		public void Format_Numeric_Fields()
+		{
+
+			var data = new Dictionary<string, object>
+			{
+				{ "prices", new List<string> {"123", "12345.0001"}}
+			};
+
+			var source = new VariableSource(data);
+
+			Assert.AreEqual("12,345.00", source.GetVariable<string>("prices.[1](N2)"));
+		}
+	}
 }
